ec8d8c8e0c7907246bcca8dc94e22cb75ed7f5b0
Anuj171018 <anujshrivastava2022@gmail.com>
2024-12-04 19:57:28 +0530

    Success

--- first.js
+++ first.js
@@ -1,1 +1,3 @@
 console.log("Anuj Shrivastava")
+alert("Hey this my site")
+document.write("Welcome to my page")
---
+++ var.js
@@ -0,0 +1,20 @@
+// var a = 10
+// var a = 12
+
+// console.log(a);
+
+// let b = 55
+// b = 60
+
+// console.log(b);
+
+// const c = 90
+// console.log(c);
+
+// ===================================================================================
+
+let num_1 = 20
+let num_2 = 50
+
+console.log("Addition of 2 number is ",num_1 + num_2);
+
