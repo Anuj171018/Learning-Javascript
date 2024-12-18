53fcf3c0a22567fa3d567a2b4255be6198a195f5
Anuj171018 <anujshrivastava2022@gmail.com>
2024-12-18 14:48:36 +0530

    Success

---
+++ dom/img.js
@@ -0,0 +1,28 @@
+function fun1() {
+    let img = document.getElementById("chimg")
+    img.src = "p1.png"
+}
+function fun2() {
+    let img = document.getElementById("chimg")
+    img.src = "p2.png"
+}
+function fun3() {
+    let img = document.getElementById("chimg")
+    img.src = "p3.png"
+}
+function fun4() {
+    let img = document.getElementById("chimg")
+    img.src = "p4.png"
+}
+function fun5() {
+    let img = document.getElementById("chimg")
+    img.src = "p5.png"
+}
+function fun6() {
+    let img = document.getElementById("chimg")
+    img.src = "p6.png"
+}
+function fun7() {
+    let img = document.getElementById("chimg")
+    img.src = "p7.png"
+}
