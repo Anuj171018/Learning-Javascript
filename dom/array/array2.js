5bd3637859311398af4e0a1bc0a3f1cd2cbdada4
Anuj171018 <anujshrivastava2022@gmail.com>
2024-12-30 19:49:52 +0530

    Success

---
+++ dom/array/array2.js
@@ -0,0 +1,15 @@
+// let ar1 = [1,2,3]
+// let ar2 = [4,5,6]
+// let ar3 = [...ar1,...ar2]
+// // console.log(ar3);
+
+let ar = [1,2,3,4,5,6,7,8,9]
+let del = ar.slice(1,5)
+
+console.log(del);
+// console.log(ar);
+
+ ar.splice(1,4,22,55,66,77,"Anuj")
+console.log(ar);
+
+
