04e0087a1f917a6f6e2563607641ebcc2c7d136d
Anuj171018 <anujshrivastava2022@gmail.com>
2025-01-07 19:50:08 +0530

    Success

--- dom/array/array3.js
+++ dom/array/array3.js
@@ -1,6 +1,6 @@
 let ar = [1,2,3,4,5,6,7,8,9,10]
 
-// let sl = ar.slice(1,5,)
+//  let sl = ar.slice(1,5,)
 // console.log(sl);
 
 // ar.splice(0,5,22,223)
---
+++ dom/array/string.js
@@ -0,0 +1,29 @@
+let str = "Anuj \n shrivastava"
+console.log(str);
+
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+
+let ct = str.repeat(5)
+
+console.log(ct );
+//Template litrals ``
+
+let tr = "Shrivastava"
+
+let str1 = `Hello 
+I am Anuj ${tr}
+I am a developer 
+`
+
+console.log(str1);
+
+let fname = "Anuj"
+let lname = "Shrivastava"
+// console.log(fname+lname);
+
+let name = fname.concat(" Mohan ",lname)
+// let name = fname.concat(lname)
+console.log(name);
+
+
