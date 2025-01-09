9f27157be79892ed8d3e7904fd7c004e4e7cb0ed
Anuj171018 <anujshrivastava2022@gmail.com>
2025-01-09 19:54:00 +0530

    Success

---
+++ dom/array/state.js
@@ -0,0 +1,8 @@
+function fun(){
+    let num1=Math.trunc(Math.random()*100+1)
+    let num2=Math.trunc(Math.random()*100+1)
+    let num3=Math.trunc(Math.random()*100+1)
+    document.querySelector("#head1").innerHTML=num1
+    document.querySelector("#head2").innerHTML=num2
+    document.querySelector("#head3").innerHTML=num3
+}
--- dom/practice/chair.js
+++ dom/practice/chair.js
@@ -1,43 +1,49 @@
-let total1 
-let total2 
+
 function fun1() {
-    total1=total1+350
+    let n=parseInt(prompt("Enter the number of chair : "))
     let a= document.querySelector("#a")
     a.classList.toggle("first")
+    document.querySelector("#total").innerHTML=n*350
 }
 function fun2() {
-    total2=total2+250
+    let n=parseInt(prompt("Enter the number of chair : "))
     let b= document.querySelector("#b")
     b.classList.toggle("second")
+    document.querySelector("#total").innerHTML=n*250
 }
 function fun3() {
-    total2=total2+250
+    let n=parseInt(prompt("Enter the number of chair : "))
     let c= document.querySelector("#c")
     c.classList.toggle("second")
+    document.querySelector("#total").innerHTML=n*250
 }
 function fun4() {
-    total1=total1+350
+    let n=parseInt(prompt("Enter the number of chair : "))
     let d= document.querySelector("#d")
     d.classList.toggle("first")
+    document.querySelector("#total").innerHTML=n*350
 }
 function fun5() {
-    total1=total1+350
+    let n=parseInt(prompt("Enter the number of chair : "))
     let e= document.querySelector("#e")
     e.classList.toggle("first")
+    document.querySelector("#total").innerHTML=n*350
 }
 function fun6() {
-    total2=total2+250
+    let n=parseInt(prompt("Enter the number of chair : "))
     let f= document.querySelector("#f")
     f.classList.toggle("second")
+    document.querySelector("#total").innerHTML=n*250
 }
 function fun7() {
-    total2=total2+250
+    let n=parseInt(prompt("Enter the number of chair : "))
     let g= document.querySelector("#g")
     g.classList.toggle("second")
+    document.querySelector("#total").innerHTML=n*250
 }
 function fun8() {
-    total1=total1+350
+    let n=parseInt(prompt("Enter the number of chair : "))
     let h= document.querySelector("#h")
     h.classList.toggle("first")
+    document.querySelector("#total").innerHTML=n*350
 }
-document.querySelector("#total").innerHTML = total1+total2
