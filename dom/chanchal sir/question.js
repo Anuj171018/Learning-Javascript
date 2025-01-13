2eaa8a736e688b58024aec06606b2eca66f1c50d
Anuj171018 <anujshrivastava2022@gmail.com>
2025-01-13 19:37:08 +0530

    Success

---
+++ dom/chanchal sir/question.js
@@ -0,0 +1,14 @@
+let s = document.querySelector("#array")
+let member = document.querySelector("#member")
+
+function fun1(){
+    let arr = []
+    for(let a =0;a<s;a++){
+        ar[a]=parseInt(document.querySelector("#member").value)
+    }
+
+    for (let v of arr){
+        document.querySelector("#head").innerHTML=v 
+        
+    }
+}
