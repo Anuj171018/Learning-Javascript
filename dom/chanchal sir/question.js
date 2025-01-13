8f5f1df7ed3f4d91918f688addeadc640dbd7c7f
Anuj171018 <anujshrivastava2022@gmail.com>
2025-01-13 19:59:55 +0530

    Success

--- dom/chanchal sir/question.js
+++ dom/chanchal sir/question.js
@@ -1,14 +1,24 @@
-let s = document.querySelector("#array")
-let member = document.querySelector("#member")
+
 
 function fun1(){
-    let arr = []
-    for(let a =0;a<s;a++){
-        ar[a]=parseInt(document.querySelector("#member").value)
-    }
+    let ip1=document.querySelector("#ip1").value
+    let ip2=document.querySelector("#ip2").value
+    let ip3=document.querySelector("#ip3").value
+    let ip4=document.querySelector("#ip4").value
+    let ip5=document.querySelector("#ip5").value
 
-    for (let v of arr){
-        document.querySelector("#head").innerHTML=v 
-        
+    let arr = [ip1,ip2,ip3,ip4,ip5]
+
+    for(let i= 0;i<5;i++){
+        let n =arr[i]
+        for (let j =1;j<=n;j++){
+            if(n%j==0){
+                console.log(j);
+                
+            }
+            
+            
+        }
+        console.log("=======================================================================");
     }
 }
