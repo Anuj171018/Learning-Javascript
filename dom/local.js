8068ff5e52af38cce2753c83746ffb4388dd2788
Anuj171018 <anujshrivastava2022@gmail.com>
2025-01-23 19:54:29 +0530

    Success

---
+++ dom/local.js
@@ -0,0 +1,12 @@
+localStorage.setItem("Name","Anuj")
+localStorage.setItem("Course","FSD")
+localStorage.setItem("Moblie","8516868273")
+localStorage.setItem("Address","Ashoka garden")
+localStorage.setItem("Email","Anuj@123gmail.com")
+
+// let name =localStorage.getItem("Name")
+// document.write("<h1>"+name+"</h1>")
+
+// localStorage.removeItem("Course")
+
+localStorage.clear()
