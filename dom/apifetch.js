3a722098fe2fa9c7f94b23d1e86d8bc902f75ca3
Anuj171018 <anujshrivastava2022@gmail.com>
2025-01-29 19:32:23 +0530

    Success

--- dom/apifetch.js
+++ dom/apifetch.js
@@ -1,7 +1,7 @@
 async function fetdata(){
     let data = await fetch("https://jsonplaceholder.typicode.com/posts")
     let res = await data.json()
-    console.log(res);
+    console.log(res); 
     
 }
 
